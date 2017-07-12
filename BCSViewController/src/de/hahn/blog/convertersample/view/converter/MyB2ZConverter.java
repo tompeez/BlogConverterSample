93ab08fb66b14ec350d40af70b0ebcfb10059d6b
Timo Hahn <tompeez@gmail.com>
2017-07-12 17:26:53 +0200

    added implementation of hte getAsObject(...) method

--- BCSViewController/src/de/hahn/blog/convertersample/view/converter/MyB2ZConverter.java
+++ BCSViewController/src/de/hahn/blog/convertersample/view/converter/MyB2ZConverter.java
@@ -9,13 +9,30 @@ public class MyB2ZConverter implements Converter {
         super();
     }
 
+    /** Method which get the data from a uiComponent and should return it in the format we like to store in the DB (or elswhere)
+     * @param facesContext current facesContext
+     * @param uIComponent component which was used to deliver the data
+     * @param string data from the ui component
+     * @return object to use for further work (e.g. storage in the DB)
+     */
     public Object getAsObject(FacesContext facesContext, UIComponent uIComponent, String string) {
-        return string;
+        if (string != null) {
+            String ret = string.replaceAll("-Z-", "B");
+            return ret;
+        } else {
+            return null;
+        }
     }
 
+    /** Method to get the string representation of hte object to use in the UI
+     * @param facesContext current facesContext
+     * @param uIComponent component which was used to deliver the data
+     * @param object data from storage to be converted
+     * @return sting to use for in the UI
+     */
     public String getAsString(FacesContext facesContext, UIComponent uIComponent, Object object) {
         if (object != null) {
-            String ret = object.toString().replaceAll("B", "Z");
+            String ret = object.toString().replaceAll("B", "-Z-");
             return ret;
         } else {
             return null;
